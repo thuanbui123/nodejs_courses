bb99428ed1f69bdcc4697c9dfd1987a3c99a9a01
Thuanbui <thuanbui31819@gmail.com>
2023-11-11 18:57:53 +0700

    create, edit, delete course

--- src/index.js
+++ src/index.js
@@ -1,26 +1,62 @@
-const path = require("path");
-const express = require("express");
-const morgan = require("morgan");
-const { engine } = require("express-handlebars");
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const methodOverride = require('method-override');
+const { engine } = require('express-handlebars');
+
+const route = require('./routes');
+const db = require('./config/db');
+
+// Connect to DB
+db.connect();
+
 const app = express();
 const port = 3000;
 
-app.use(express.static(__dirname + "/public"));
+// Use static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+);
+app.use(express.json());
+
+app.use(methodOverride('_method'));
 
 // HTTP logger
-app.use(morgan("combined"));
+// app.use(morgan('combined'));
 
 // Template engine
-app.engine("hbs", engine({
-  extname: '.hbs'
-}));
-app.set("view engine", "hbs");
-app.set("views", path.join(__dirname, "resources/views"));
+// app.engine(
+//     'hbs',
+//     handlebars({
+//         extname: '.hbs',
+//         helpers: {
+//             sum: (a, b) => a + b,
+//         },
+//     }),
+// );
 
-app.get("/", (req, res) => {
-  res.render("home");
-});
+app.engine(
+    'hbs',
+    engine({
+        extname: '.hbs',
+        helpers: {
+            sum: (a, b) => a + b,
+        },
+    }),
+);
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources', 'views'));
+
+// Routes init
+route(app);
+
+app.listen(port, () =>
+    console.log(`App listening at http://localhost:${port}`),
+);
